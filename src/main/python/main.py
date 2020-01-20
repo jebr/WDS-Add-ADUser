e964eefd917bd3473fc55f2e126150aa96ffd5a7
Jeroen Brauns <brauns.jeroen@pm.me>
2020-01-20 14:36:39 +0100

    Inititial setup

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -1,12 +1,30 @@
 from fbs_runtime.application_context.PyQt5 import ApplicationContext
-from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtWidgets import QMainWindow, QApplication, QLabel, QFileDialog, QMessageBox
 
+from PyQt5.uic import loadUi
+from PyQt5 import QtWidgets, QtGui, QtCore, uic
+from PyQt5.QtGui import QPixmap,  QFont
 import sys
+import os
+import logging
+
+# Set logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+# logging.disable(logging.DEBUG)
+
+Form, Window = uic.loadUi("main_window.ui")
+
+
+def main():
+    app = QApplication([])
+    window = Window()
+    form = Form()
+    form.setupUi(window)
+    window.resize(640, 480)
+    window.setWindowTitle('Windows Domain Settings')
+    window.show()
+    sys.exit(app.exec())
+
 
 if __name__ == '__main__':
-    appctxt = ApplicationContext()       # 1. Instantiate ApplicationContext
-    window = QMainWindow()
-    window.resize(250, 150)
-    window.show()
-    exit_code = appctxt.app.exec_()      # 2. Invoke appctxt.app.exec_()
-    sys.exit(exit_code)
+    main()
